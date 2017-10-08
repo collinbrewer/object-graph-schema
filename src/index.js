f355b4c4c2b80c4435db9041b659087bddb5b619
Collin Brewer <collin@brewer-collective.com>
2017-10-08 10:10:06 -0500

    chore: rework folder structure

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ global.Expression = require('@collinbrewer/expression');
 global.Predicate = require('@collinbrewer/predicate');
 global.Descriptor = require('@collinbrewer/descriptor');
 
-var ObjectGraph = require('./src/schema-definitions/index.js');
+var ObjectGraph = require('./schema-definitions/index.js');
 
 module.exports = ObjectGraph;
